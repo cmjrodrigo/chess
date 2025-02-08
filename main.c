852374e5956abd7445ef1387220bfd5365ab8663
Jose Rodrigo Cisneros Murillo <cmjrodrigo53@gmail.com>
2025-02-07 23:07:58 -0600

    Adds code for pawn and knight pieces (removed most of all old code), and a way to check for the validity of a move

--- main.c
+++ main.c
@@ -1,10 +1,85 @@
+#include "pieces.h"
+#include <string.h>
 #include <stdio.h>
-#include "board.h"
+#include <wchar.h>
+#include <locale.h>
 
-extern square board[8][8];
+void InteractiveCheckMovementValidity()
+{
 
-int main(void){
-  b_init();
-  
-  b_print();
+  //Get piece info
+  printf("Please enter a piece(p, N): ");
+  char p = getchar();
+  getchar();
+
+  printf("Please enter a coordinate: ");
+  char coor[3];
+  scanf("%s", coor);
+
+  printf("Please enter a destination: ");
+  char dest[3];
+  scanf("%s", dest);
+
+  printf("Choose a color (Black = -1, White = 1): ");
+  int color;
+  scanf("%d", &color);
+
+  char movtype[20];
+  int validity;
+
+  Piece piece;
+  //Process piece info and set print message parameters
+  if(p == 'p')
+  {
+    piece = Pawn(color, coor);
+    if((validity = IsMovementValid(dest, &piece)) > -1)
+    {
+      if(piece.movset[validity].attk == 1)
+      {
+        strcpy(movtype, "attack");
+      }
+      else if(piece.movset[validity].attk == 0)
+      {
+        strcpy(movtype, "move");
+      }
+    }
+    else
+    {
+      strcpy(movtype, "attack/move");
+    }
+  }
+  else if( p == 'N')
+  {
+    
+    piece = Knight(color, coor);
+    if((validity = IsMovementValid(dest, &piece)) > -1)
+    {
+      if(piece.movset[validity].attk == 1)
+      {
+        strcpy(movtype, "attack");
+      }
+      else if(piece.movset[validity].attk == 0)
+      {
+        strcpy(movtype, "move");
+      }
+    }
+    else
+    {
+      strcpy(movtype, "attack/move");
+    }
+  }
+
+  printf("%s %s %s to %s\n", piece.pos, validity >= 0 ? "can" : "can't", movtype, dest);
+
+}
+
+int main(int argc, char *argv[])
+{
+  setlocale(LC_ALL, "");
+
+  for(;;)
+  {
+    InteractiveCheckMovementValidity();
+  }
+
 }
--- pieces.h
+++ pieces.h
@@ -1,9 +1,105 @@
 #ifndef PIECES_H
 #define PIECES_H
 
-typedef struct{
-	char *name;
-	int movset
-}piece;
-;
+#include <wchar.h>
+#include <locale.h>
+#include <string.h>
+#include <stdio.h>
+
+#define BLACK -1
+#define WHITE 1
+
+typedef struct
+{
+  unsigned short max;       //If max == 1 can be any x or y value from 1 to x/yVal. If not, the movement will be exactly as xVal and yVal
+  short attk;               //0 for move only, 1 for attack only, -1 for both
+  short int xVal;
+  short int yVal;
+} Movement;
+
+typedef struct
+{
+  unsigned short color;      //Color of the piece to detemrine movement
+  char pos[3];          //Current position of the piece
+  wchar_t symbol;            //Shown symbol of the piece
+  unsigned short movlen;
+  Movement movset[8];      //Array of possible moves per piece
+} Piece;
+
+
+//Checks if the movement given is one that the piece can do.
+//Returns an index if valid and -1 if not
+int IsMovementValid(char *destination, Piece *piece)
+{
+  int valid = -1;
+  char *position = piece -> pos;
+
+  //Calculation of X an Y offset
+#ifdef DEBUG
+  printf("X = %c - %c\n", *destination, *position);
+#endif
+  int x_offset = *(destination ++) - *(position++);
+#ifdef DEBUG
+  printf("Y = %c - %c\n", *destination, *position);
+#endif
+  int y_offset = *destination - *position;
+  
+  //Reads the movset array and compares it to the possible X and Y values
+  for(int i = 0; i < piece -> movlen; i++)
+  {
+    if((piece -> movset[i].xVal == x_offset) && (piece -> movset[i].yVal == y_offset))
+    {
+      valid = i;
+      break;
+    }
+  }
+
+  return valid;
+}
+
+
+
+Piece Pawn(unsigned short c, char p[3])
+{
+  Piece pawn = {
+    c,
+    "  ",
+    L'p',
+    3,
+    {
+    //max, attk, x, y
+      {0, 1, 1, 1 * (int) c},
+      {0, 1, -1, 1 * (int) c},
+      {0, 0, 0, 1 * (int) c}
+    }
+  };
+  
+  strcpy(pawn.pos, p);
+  return pawn;
+}
+
+Piece Knight(unsigned short c, char p[3])
+{
+  Piece knight ={
+    c,
+    "  ",
+    L'N',
+    8,
+    {
+    // max, attk, x, y
+      {0, -1, 2, 1},
+      {0, -1, 2, -1},
+      {0, -1, -2, 1},
+      {0, -1, -2, -1},
+      {0, -1, 1, 2},
+      {0, -1, -1, 2},
+      {0, -1, 1, -2},
+      {0, -1, -1, -2}
+    }
+  };
+
+  strcpy(knight.pos, p);
+  return knight;
+}
+
 #endif
